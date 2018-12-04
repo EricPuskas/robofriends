c447643cc2933b83a8e6fb2b0a260ddb9ee0d463
EricPuskas <eric.puskas@yahoo.com>
2018-12-04 19:23:22 +0200

    Updates

---
+++ precache-manifest.a55ec04b659a878bf7f486dafdf4a727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c16ca397ca36028f946f828e788ad204",
+    "url": "/robofriends/static/media/robotaur.c16ca397.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "966fb4849327c60200c9",
+    "url": "/robofriends/static/js/main.966fb484.chunk.js"
+  },
+  {
+    "revision": "cf4b2cdc210fe97970a2",
+    "url": "/robofriends/static/js/1.cf4b2cdc.chunk.js"
+  },
+  {
+    "revision": "966fb4849327c60200c9",
+    "url": "/robofriends/static/css/main.d7898995.chunk.css"
+  },
+  {
+    "revision": "cf4b2cdc210fe97970a2",
+    "url": "/robofriends/static/css/1.df9257b5.chunk.css"
+  },
+  {
+    "revision": "5ec15acc3c8168251336cb53d4193742",
+    "url": "/robofriends/index.html"
+  }
+];
